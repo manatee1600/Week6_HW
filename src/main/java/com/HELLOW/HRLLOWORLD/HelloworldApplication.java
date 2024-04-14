b48911bdecbd747c19914f7be77bbb1abc63b16b
elephant_seal <mikan.ha.orange123@gmail.com>
2024-04-14 10:58:48 +0900

    提出

--- src/main/java/com/HELLOW/HRLLOWORLD/HelloworldApplication.java
+++ src/main/java/com/HELLOW/HRLLOWORLD/HelloworldApplication.java
@@ -3,8 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrlloworldApplication {
+public class HelloworldApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(HrlloworldApplication.class, args);
+		SpringApplication.run(HelloworldApplication.class, args);
 	}
 }
